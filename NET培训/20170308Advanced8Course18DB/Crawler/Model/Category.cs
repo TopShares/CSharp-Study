edc6b312d8807bbebd01753bf7c358299d6b8e73
TopShares <1255877369@qq.com>
2019-08-02 07:33:10 +0800

    .

---
+++ NET培训/20170308Advanced8Course18DB/Crawler/Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Model
+{
+    public class Category:BaseModel
+    {
+        public string Code { get; set; }
+        public string ParentCode { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int CategoryLevel { get; set; }
+        public int State { get; set; }
+    }
+}
